eb18ee7e387f2151b59a41830b162a334de69bad
edinvd <edinvasquez412@gmail.com>
2021-06-14 14:58:20 -0500

    Creamos la suma en python

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print("Hello World")
+
+""" Creamos la suma """
+a = int (input("Ingrese primer numero: "))
+b = int (input("Ingrese segundo numero numero: "))
+suma = a + b
+
+print("La suma es: ", suma)
